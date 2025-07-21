e8f891e1035575d588bf4546ea5bf45b75254bc0
Iris Xu <xuiris142@gmail.com>
2025-07-20 21:28:38 -0600

    add three buttons to record player's choice and update messages

--- javascript.js
+++ javascript.js
@@ -13,39 +13,39 @@ function getHumanChoice(){
     return prompt("Choose between rock, paper, and scissors: ");
 };
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log("You guys tied it up!");
-        } else if ((humanChoice === "rock" && computerChoice === "scissors")
-        || (humanChoice === "scissors" && computerChoice === "paper")
-        || (humanChoice === "paper" && computerChoice === "rock")) {
-            console.log(`You win -- ${humanChoice} beats ${computerChoice}!`);
-            humanScore ++;
-        } else {
-            console.log(`You lose -- ${humanChoice} loses to ${computerChoice}.`);
-            computerScore ++;
-        }
-    } 
-    
-    for (let i = 0; i < 5; i++){
-        let humanChoice = getHumanChoice().toLowerCase();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
+const contain = document.querySelector("#container");
+const results = document.querySelector(".results");
+results.textContent = `you VS. computer = ${humanScore} : ${computerScore}`;
 
-    if(humanScore > computerScore){
-        console.log("Hooray! Overall you win!");
-    } else if (humanScore < computerScore){
-        console.log("Sorry! Overall you lose. You can win next time.");
+const messages = document.createElement("div");
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        messages.textContent = "You guys tied it up!";
+    } else if ((humanChoice === "rock" && computerChoice === "scissors")
+    || (humanChoice === "scissors" && computerChoice === "paper")
+    || (humanChoice === "paper" && computerChoice === "rock")) {
+        messages.textContent = `Computer plays ${computerChoice}. You win -- ${humanChoice} beats ${computerChoice}!`
+        humanScore ++;
     } else {
-        console.log("Wow! Overall you guys tie up.")
+        messages.textContent = `Computer plays ${computerChoice}. You lose -- ${humanChoice} loses to ${computerChoice}.`;
+        computerScore ++;
     }
-}
+    results.textContent = `you VS. computer = ${humanScore} : ${computerScore}`;
+    contain.appendChild(messages);
+} 
 
 
-playGame()
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const humanChoice = button.id;
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+
+    });
+})
 
